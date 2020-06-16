5dd62c702cae8880721305659c2e7ded0af02bec
DrewTittle <drew.tittle@gmail.com>
2020-06-16 08:56:06 -0600

    first commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+  @GetMapping(path = "/")
+  public String home() {
+    return "Hello talentReef!!!SonarLintFixes!";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
---
+++ src/test/java/hello/ApplicationTest.java
@@ -0,0 +1,11 @@
+package hello;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+    @Test public void testAppHasAGreeting() {
+        Application classUnderTest = new Application();
+        assertNotNull("app should have a greeting", classUnderTest.home());
+    }
+}
